e0df2398ed735df2bd204b4f3525e45ef201e8ec
Stéphane <stephane1920@gmail.com>
2021-11-02 18:10:07 +0100

    favicon

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,500;0,700;0,900;1,100;1,200;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="./styles/style.css"  rel="stylesheet">  
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/style.css"  rel="stylesheet">
+  <link rel="icon" href="styles/favicon.ico" />
+  <link rel="icon" type="image/png" href="styles/favicon-32x32.png" />
   <title>CaseTonPote - service de rencontres</title>
 </head>
 <body class="px-5r">
--- informations.php
+++ informations.php
@@ -9,7 +9,9 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,500;0,700;0,900;1,100;1,200;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="./styles/style.css"  rel="stylesheet">   
+  <link rel="stylesheet" href="./styles/style.css"  rel="stylesheet">
+  <link rel="icon" href="styles/favicon.ico" />
+  <link rel="icon" type="image/png" href="styles/favicon-32x32.png" />
   <title>CaseTonPote - service de rencontres</title>
 </head>
 <body class="px-5r">
