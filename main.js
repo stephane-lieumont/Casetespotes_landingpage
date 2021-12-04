2510e7b4821f7a9f2b2de7c70f8b87d3af97aaf3
Stéphane <stephane1920@gmail.com>
2021-12-04 15:07:26 +0100

    clean folder due to error

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('body').classList.add('loading')
-document.onreadystatechange = function () {
-  if(document.readyState == "complete") {
-    document.querySelector('body').classList.remove('loading')
-    document.querySelector('body').classList.add('load')
-  }
-}
