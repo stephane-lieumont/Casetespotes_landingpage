88075d9355d09ea9a9e93eace84ce561f6f13090
Stéphane <stephane1920@gmail.com>
2022-04-22 16:49:56 +0200

    fix:layout problem scroll

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from './App';
+import App from './app';
 
 import './sass/main.scss'
 
