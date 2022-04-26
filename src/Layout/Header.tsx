5a45df2eb7837067bb3ca1aba89b8eb4b3013df3
Stéphane <stephane1920@gmail.com>
2022-04-26 09:40:12 +0200

    add hamberger menu

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,13 +1,11 @@
 import { FunctionComponent, useState, useEffect } from "react"
 import Logo from '../assets/logo.svg'
 import { NavLink, Link } from 'react-router-dom'
-
-type HeaderProps = {
-  minifyHeader?:boolean
-}
+import { HeaderProps } from "../types/interfaceComponents"
 
 const Header: FunctionComponent<HeaderProps> = ({minifyHeader = false}) => {
   const [minify, setMinify] = useState<boolean>(minifyHeader)
+  const [showMenu, setShowMenu] = useState<boolean>(false)
 
   useEffect(() => {
     setMinify(minifyHeader)
@@ -19,10 +17,15 @@ const Header: FunctionComponent<HeaderProps> = ({minifyHeader = false}) => {
         <svg><use xlinkHref={Logo + '#logo'}></use></svg>
       </Link>
       <nav>
+        <div onClick={() => setShowMenu(!showMenu)} className={`hamburger ${ showMenu ? 'is-active' : '' }`}>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
         <ul>          
-          <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/a-propos">A propos</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/contact">Contact</NavLink></li>
-          <li><NavLink className={({ isActive }) => (isActive ? 'active' : '')} to="/">Pré-inscription</NavLink></li>
+          <li><NavLink onClick={() => setShowMenu(false)} className={({ isActive }) => (isActive ? 'active' : '')} to="/a-propos">A propos</NavLink></li>
+          <li><NavLink onClick={() => setShowMenu(false)} className={({ isActive }) => (isActive ? 'active' : '')} to="/contact">Contact</NavLink></li>
+          <li><NavLink onClick={() => setShowMenu(false)} className={({ isActive }) => (isActive ? 'active' : '')} to="/">Pré-inscription</NavLink></li>
         </ul>
       </nav>
     </header>
