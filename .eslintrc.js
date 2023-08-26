f6c9295d67df196a4ce3756f0252fa02545cac66
Stephane Lieumont <stephane1920@gmail.com>
2023-08-26 16:34:03 +0200

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   overrides: [
     {
       env: {
